36c1a4810d201533b79977534ff2d16a5cbf8dbb
ArtemRasolka <artem.mellon@mail.ru>
2022-08-16 19:33:55 +0300

    ALayout

---
+++ ALayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ALayout
+//
+//  Created by Admin on 16.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
